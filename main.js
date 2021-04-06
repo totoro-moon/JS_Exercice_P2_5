58f6374af55ca5135693dbc10467c65e5affa47b
FLAVIE <flavie@MacBook-Pro-de-FLAVIE.local>
2021-04-06 16:38:15 +0200

    JS_Exercice_P2_5

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
 //on crée une fonction pour changer d'image au passage de la souris (onmouseover dans le html)
 //  en lui mettant dans les () un paramètre
-function changePic(image){
-   // le paramètre devient un argument que l'on concatènne avec l'url et la fin d'extension des images
-   document.getElementById(image).src = "/Users/flavie/Desktop/formation/js/JS_Exercice_P2_5 /images/" + image + "_2.jpg"
-   
+// le paramètre devient un argument que l'on concatènne avec l'url et la fin d'extension des images
+
+function changePics(image) {
+   document.getElementById(image).src = "/Users/flavie/Desktop/formation/js/JS_Exercice_P2_5/images/" + image + "_2.jpg"
 }
-
-
-
